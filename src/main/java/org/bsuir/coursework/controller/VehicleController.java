9b1163b0dd000f075b308f600f77650e5ada75f0
katyyanka <katherinemoroshchuk@gmail.com>
2022-04-13 14:26:06 +0300

    error page, 505 page

--- src/main/java/org/bsuir/coursework/controller/VehicleController.java
+++ src/main/java/org/bsuir/coursework/controller/VehicleController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Controller
 public class VehicleController {
+
     private final VehicleService vehicleService;
 
     @Autowired
@@ -37,6 +38,6 @@ public class VehicleController {
     @PostMapping("/vehicle-create")
     public String createBus(Vehicle vehicle){
         vehicleService.saveVehicle(vehicle);
-        return "redirect:/buses";
+        return "redirect:/vehicles";
     }
 }
--- src/main/java/org/bsuir/coursework/entity/Vehicle.java
+++ src/main/java/org/bsuir/coursework/entity/Vehicle.java
@@ -1,28 +1,25 @@
 package org.bsuir.coursework.entity;
 
 import lombok.Data;
-import lombok.ToString;
 
 import javax.persistence.*;
 
 @Data
 @Entity
-@ToString
 @Table(name="vehicle")
 public class Vehicle {
     @Id
+    @Column(name="number")
     private String number;
     @Column(name="NUMBER_OF_SETS")
     private int numberOfSets;
     private String brand;
     @Column(name="FUEL_CONSUMPTION_PER_100_KILOMETERS")
     private int fuelConsumptionPer100Kilometers;
-    /*@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "DRIVER_EMAIL", referencedColumnName = "USERNAME")*/
-    @OneToOne
-    @MapsId
-    @JoinColumn(name = "driver_email")
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "DRIVER_EMAIL", referencedColumnName = "USERNAME")
     private User user;
-    @Column(name="FUEL_NAME")
-    private String fuel;
+    @ManyToOne
+    @JoinColumn(name="FUEL_NAME")
+    private Fuel fuel;
 }
